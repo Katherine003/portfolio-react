dc8623e83b951c5a0d43f1af560e3faa8fa23e97
Kat <katherine.howe@sckans.edu>
2022-10-13 12:30:17 -0500

    fix

---
+++ docs/precache-manifest.58b776a137f93f9a36b0b8063f8e91ba.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dd23884c31f7e8cf164d66d297c2fa26",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4dd464a1fca9a7076155",
+    "url": "/static/css/main.e47ddcae.chunk.css"
+  },
+  {
+    "revision": "ac27f4e7dc2934ee0c86",
+    "url": "/static/js/2.fae6ec37.chunk.js"
+  },
+  {
+    "revision": "d5b473ff893d6b5fac7d0b5081a5072a",
+    "url": "/static/js/2.fae6ec37.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4dd464a1fca9a7076155",
+    "url": "/static/js/main.cbdd16f2.chunk.js"
+  },
+  {
+    "revision": "f1a256b8ca68736efd22",
+    "url": "/static/js/runtime-main.1ddc2d8d.js"
+  },
+  {
+    "revision": "3ce357e71a836a86691b904396a8955c",
+    "url": "/static/media/cta-illustration.3ce357e7.svg"
+  },
+  {
+    "revision": "83695e6f76b2b6f27b53fdd7e2cc89e4",
+    "url": "/static/media/favorite_FILL0_wght400_GRAD0_opsz48.83695e6f.svg"
+  },
+  {
+    "revision": "0f9928d77bc67692e90527ec092fcbd0",
+    "url": "/static/media/feature-tile-icon-01.0f9928d7.svg"
+  },
+  {
+    "revision": "66f37ba5c7e4b6648f04883fa637e634",
+    "url": "/static/media/feature-tile-icon-03.66f37ba5.svg"
+  },
+  {
+    "revision": "836acd100a942420f8ab4e6c5e4162d8",
+    "url": "/static/media/feature-tile-icon-04.836acd10.svg"
+  },
+  {
+    "revision": "fa9ba00b178abc7bd2e02bd986e91fe3",
+    "url": "/static/media/feature-tile-icon-05.fa9ba00b.svg"
+  },
+  {
+    "revision": "6a1776963deb39d710bae80bb449aebe",
+    "url": "/static/media/feature-tile-icon-06.6a177696.svg"
+  },
+  {
+    "revision": "87e4d053756c60c9f421195d8eafdbfe",
+    "url": "/static/media/features-split-image-03.87e4d053.png"
+  },
+  {
+    "revision": "28eaa2093a9c294da9f0e3cffe85f444",
+    "url": "/static/media/gib.28eaa209.jpg"
+  },
+  {
+    "revision": "ea6949039d6554ebf2cf109f333f5811",
+    "url": "/static/media/illustration-section-01.ea694903.svg"
+  },
+  {
+    "revision": "63d0555d90c053eea1a1a7469d004661",
+    "url": "/static/media/illustration-section-02.63d0555d.svg"
+  },
+  {
+    "revision": "2810a88b1d6365b363caeabe678f1629",
+    "url": "/static/media/logo.2810a88b.svg"
+  },
+  {
+    "revision": "006de90bffc6672085b5a09d435be629",
+    "url": "/static/media/moth.006de90b.png"
+  },
+  {
+    "revision": "a622fc5dd8adcdd27ea42218385013b4",
+    "url": "/static/media/video-placeholder.a622fc5d.jpg"
+  }
+]);
